1a98d659288d15e881de664b710a1ef57bd00ea5
Devika Palande <42401144+D-Palande@users.noreply.github.com>
2021-08-18 18:49:54 +0530

    Update User.java

--- elastic-example-1/src/main/java/com/elastic/entity/User.java
+++ elastic-example-1/src/main/java/com/elastic/entity/User.java
@@ -1,6 +1,12 @@
 package com.elastic.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
 	
@@ -8,39 +14,6 @@ public class User {
 	private String userName;
 	private String address;
 
-	public User() {
-		super();
-	}
-
-	public User(String userId, String userName, String address) {
-		super();
-		this.userId = userId;
-		this.userName = userName;
-		this.address= address;
-		
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+	
 }
+
